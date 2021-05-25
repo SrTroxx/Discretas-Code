b10449af527f0c6e9e121ca80876750a3281b6ce
William Camilo Galindo Bustos <wcgalindob@gmail.com>
2021-05-24 21:23:08 -0500

    Cambios Generar Grafo, Falta funcion general para el diseño del grafo

--- Matriz - Grafos/Program.cs
+++ Matriz - Grafos/Program.cs
@@ -9,7 +9,53 @@ namespace Matriz___Grafos
     {
         static void Main(string[] args)
         {
-            //la mala daniel
+
+        
+
+        }
+
+
+        public static void logicaGrafo(int x, int y)
+        {
+
+
+            //Vertice nuevo = new Vertice(x);
+
+            //foreach (var item in nuevo.ToString())
+            //{
+
+            //}
+
+            //Vertice oVertice1 = new Vertice(1);
+            //Vertice oVertice2 = new Vertice(2);
+            //Vertice oVertice3 = new Vertice(3);
+            //Vertice oVertice4 = new Vertice(4);
+            //Vertice oVertice5 = new Vertice(5);
+            //Vertice oVertice6 = new Vertice(6);
+
+            //oVertice6.Aristas.Add(oVertice4);
+            //oVertice4.Aristas.Add(oVertice5);
+            //oVertice4.Aristas.Add(oVertice3);
+            //oVertice3.Aristas.Add(oVertice2);
+            //oVertice5.Aristas.Add(oVertice2);
+            //oVertice5.Aristas.Add(oVertice1);
+            //oVertice2.Aristas.Add(oVertice1);
+
+            //GenerarGrafo(oVertice6);
+
+        }
+
+
+        public static void GenerarGrafo(Vertice oVertice, string sangria="")
+        {
+            if (oVertice != null)
+            {
+                Console.WriteLine(sangria+oVertice.Valor);
+                foreach (var ov in oVertice.Aristas)
+                {
+                    GenerarGrafo(ov,sangria+"\t");
+                }
+            }
         }
     }
 }
---
+++ Matriz - Grafos/Vertice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matriz___Grafos
+{
+    public class Vertice
+    {
+        #region "PROPIEDADES"
+
+        public int Valor { get; set; }
+        public List<Vertice> Aristas { get; set; }
+
+
+        #region "CONSTRUCTOR"
+        public Vertice(int valor)
+        {
+
+            this.Valor = valor;
+            Aristas = new List<Vertice>();
+
+        }
+        #endregion
+
+        #endregion
+    }
+
+}
